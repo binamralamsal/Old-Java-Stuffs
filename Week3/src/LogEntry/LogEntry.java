15edc715ca788677651f19b05235416311e54ea4
Binamra-Lamsal <binamraprogrammer@gmail.com>
2020-06-06 07:37:32 +0545

    Code Changed

--- Week3/src/LogEntry/LogEntry.java
+++ Week3/src/LogEntry/LogEntry.java
@@ -46,4 +46,11 @@ public class LogEntry
         return ipAddress + " " + accessTime + " " + request
                 + " " + statusCode + " " + bytesReturned;
     }
+
+    public static void main(String[] args) {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
 }
