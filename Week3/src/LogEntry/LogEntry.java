5399f1f1f13ccbfd15d9abb997092d92d263dcac
Binamra-Lamsal <binamraprogrammer@gmail.com>
2020-06-06 07:33:10 +0545

    Added Files

--- Week3/src/LogEntry/LogEntry.java
+++ Week3/src/LogEntry/LogEntry.java
@@ -1,4 +1,49 @@
+/**
+ * Write a description of class LogRecord here.
+ *
+ * @author (Binamra Lamsal)
+ * @version (1.0)
+ */
+
+
 package LogEntry;
 
-public class LogEntry {
+import  java.util.*;
+public class LogEntry
+{
+    private String ipAddress;
+    private Date accessTime;
+    private String request;
+    private int statusCode;
+    private int bytesReturned;
+
+    public LogEntry(String ip, Date time, String req, int status, int bytes) {
+        ipAddress = ip;
+        accessTime = time;
+        request = req;
+        statusCode = status;
+        bytesReturned = bytes;
+
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+    public Date getAccessTime() {
+        return accessTime;
+    }
+    public String getRequest() {
+        return request;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
+    public int getBytesReturned() {
+        return bytesReturned;
+    }
+
+    public String toString() {
+        return ipAddress + " " + accessTime + " " + request
+                + " " + statusCode + " " + bytesReturned;
+    }
 }
